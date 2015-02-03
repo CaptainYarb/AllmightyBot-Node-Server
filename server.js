8441c4fed3d02fcd568bb661721b3b7e75a1569a
Jonathan Yarbor <jon@getnodecraft.net>
2015-02-03 09:39:25 -0600

    Restructured Code
    - Removed thrown errors (throwing errors in node.js is a no no)
    - Added STDERR (console.error) output for errors
    - Restructured code to remove repeating callback code
    - Moved listening callback to run after server is actually online
    - Removed repeating r.db() (not necessary if 'database' is set in initial config)
    - Added rethinkdb config.json file to serve as better config mechanism

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 var server = require('http').createServer(),
     io = require('socket.io')(server),
     env = require('node-env-file'),
-    r = require('rethinkdb');
+    r = require('rethinkdb'),
+    rethinkConfig = require('config.json');
 
-env(__dirname + '/.env');
-
-r.connect({host: 'localhost', port: 28015}, function (err, conn) {
-    if (err) throw err;
-
-    console.log('Listening on port ' + process.env.SERVER_PORT);
+r.connect(rethinkConfig, function (err, conn) {
+    // throwing errors in node.js is a no-no
+    if(err){
+        console.error('Failed to connect to DB');
+        return process.exit();
+    }
 
     io.on('connection', function (socket) {
         console.log('Connected to socket with ID of ' + socket.id);
 
+        var dbCallback = function(err, res){
+            if(err){
+                return console.error('RQL Error:', err);
+            }
+            return console.log(res);
+        }
+
         socket.on('chat_message', function (data) {
-            console.log('Chat message event received: ' + JSON.stringify(data));
-            r.db(process.env.RETHINKDB_TABLE).table('chat_messages').insert(data).run(conn, function (err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
+            console.log('Chat message event received: ', data);
+            r.table('chat_messages').insert(data).run(conn, dbCallback);
         });
 
         socket.on('user_join', function (data) {
-            console.log('User join event received: ' + JSON.stringify(data));
-            r.db(process.env.RETHINKDB_TABLE).table('user_joins').insert(data).run(conn, function (err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
+            console.log('User join event received: ', data);
+            r.table('user_joins').insert(data).run(conn, dbCallback);
         });
 
         socket.on('user_part', function (data) {
-            console.log('User parted event received: ' + JSON.stringify(data));
-            r.db(process.env.RETHINKDB_TABLE).table('user_parts').insert(data).run(conn, function (err, res) {
-                if (err) throw err;
-                console.log(res);
-            });
+            console.log('User parted event received: ', data);
+            r.table('user_parts').insert(data).run(conn, dbCallback);
         });
 
         socket.on('disconnect', function () {
             console.log('Disconnected from socket with ID of ' + socket.id);
         });
     });
-
-    server.listen(process.env.SERVER_PORT);
+});
+server.listen(process.env.SERVER_PORT, function(){
+    console.log('Listening on port ' + process.env.SERVER_PORT);
 });
